71423a86cab4189b8a0477de1de378862f1139b0
kohei.LD <NA>
2018-04-12 14:55:09 +0000

    User model and gravatar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   get 'toppages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "toppages#index"
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
+  get "sign up", to: "users#new"
 end
